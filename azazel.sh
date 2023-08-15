50f9ae1c300a217307bd544242a7899fb588822b
YoruYagami <70035442+YoruYagami@users.noreply.github.com>
2023-08-15 14:10:33 +0200

    Update azazel.sh

--- azazel.sh
+++ azazel.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Adding some colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 template=""  # Default template
 rate_limit=""
-spider_tool="paramspider"  # Default spider tool
 katana_rate_limit=""
 proxy=""
 nikto_scan=false
@@ -24,6 +29,8 @@ help_message() {
     echo -e "\nAvailable spider tools: paramspider, katana"
     echo
 }
+
+spider_tool="paramspider"  # Default spider tool
 
 while :; do
     case $1 in
@@ -82,89 +89,92 @@ timestamp=$(date '+%Y%m%d%H%M')
 output_dir="scan/$target/$timestamp"
 mkdir -p "$output_dir"
 
-# Function to display colored messages
-color_echo() {
-    local color="$1"
-    shift
-    echo -e "\033[${color}m$@\033[0m"
-}
-
 # Essentials Check
 check_additional_info() {
     local target="$1"
     local output_dir="$2"
+    declare -a endpoints=("/robots.txt" "/sitemap.xml" "/phpinfo.php" "/admin" "/login.php" "/cgi-bin" "/dashboard")
+    declare -a source_code_endpoints=("/git" "/svn" "/hg" "/bzr" "/_darcs" "/Bitkeeper")
 
     echo
 
-    # Check /robots.txt
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$target/robots.txt")
-    if [[ $response -eq 200 ]]; then
-        color_echo 32 "[+] /robots.txt found. Saving..."
-        echo
-        # Get and display the content of robots.txt
-        robots_content=$(curl -s "$target/robots.txt")
-        echo "$robots_content"
-        echo "$robots_content" > "$output_dir/robots.txt"
-    else
-        color_echo 31 "[-] /robots.txt not found."
-    fi
+    # Use whatweb for detecting technologies 
+    echo -e "${YELLOW}[+] Detecting Technologies on the target...${NC}"
+    whatweb -a 1 "$target" --log-brief="$output_dir/whatweb.txt"
 
     echo
 
-    # Check /sitemap.xml
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$target/sitemap.xml")
-    if [[ $response -eq 200 ]]; then
-        color_echo 32 "[+] /sitemap.xml found. Saving..."
-        
-        # Save the content of sitemap.xml without displaying on the terminal
-        sitemap_content=$(curl -s "$target/sitemap.xml")
-        echo "$sitemap_content" > "$output_dir/sitemap.xml"
-    else
-        color_echo 31 "[-] /sitemap.xml not found."
-    fi
+    # Check custom endpoints
+    for endpoint in "${endpoints[@]}"; do
+        check_endpoint "$target" "$endpoint" "$output_dir"
+        sleep 1
+    done
 
     echo
+
+    # Check for Exposed Source Code
+    echo -e "${YELLOW}[+] Checking for Exposed Source Code Endpoints...${NC}"
+    for endpoint in "${source_code_endpoints[@]}"; do
+        check_endpoint "$target" "$endpoint" "$output_dir"
+        sleep 1
+    done
+
+    echo
+
+    echo -e "${YELLOW}[+] Checking for low hanging fruit${NC}"
 
     # Check X-Frame-Options header
     header=$(curl -s -I "$target" | grep -i "X-Frame-Options")
     if [[ -z $header ]]; then
-        color_echo 31 "[-] Target vulnerable to Clickjacking!."
+        echo -e "${GREEN}[+] Target vulnerable to Clickjacking!.${NC}"
     else
-        color_echo 32 "[+] Anti-clickjacking X-Frame-Options header detected."
+        echo -e "${RED}[+] Anti-clickjacking X-Frame-Options header detected.${NC}"
     fi
-
-    echo
 
     # Check for HttpOnly flag
     cookie_header=$(curl -s -I "$target" | grep -i "Set-Cookie")
     if [[ $cookie_header == *"; HttpOnly"* ]]; then
-        color_echo 32 "[+] HttpOnly flag is set on the cookie."
+        echo -e "${RED}[-] HttpOnly flag is set on the cookie.${NC}"
     else
-        color_echo 31 "[-] HttpOnly flag is missing from the cookie!."
+        echo -e "${GREEN}[+] HttpOnly flag is missing from the cookie!.${NC}"
     fi
-
-    echo
 
     # Check for Content-Security-Policy (CSP) header
     csp_header=$(curl -s -I "$target" | grep -i "Content-Security-Policy")
     if [[ -z $csp_header ]]; then
-        color_echo 31 "[-] Content-Security-Policy (CSP) header is missing!."
+        echo -e "${GREEN}[+] Content-Security-Policy (CSP) header is missing!.${NC}"
     else
         if [[ $csp_header == *'unsafe-inline'* ]] || [[ $csp_header == *'unsafe-eval'* ]]; then
-            color_echo 31 "[+] CSP contains unsafe directives (unsafe-inline or unsafe-eval)."
+            echo -e "${GREEN}[+] CSP contains unsafe directives (unsafe-inline or unsafe-eval).${NC}"
         else
-            color_echo 32 "[-] Proper CSP header detected."
+            echo -e "${RED}[-] Proper CSP header detected.${NC}"
         fi
     fi
 
     echo
 }
 
+check_endpoint() {
+    local target="$1"
+    local endpoint="$2"
+    local output_dir="$3"
+
+    response=$(curl -s -o /dev/null -w "%{http_code}" "$target$endpoint")
+    if [[ $response -eq 200 ]]; then
+        echo -e "${GREEN}[+] $endpoint found. Saving...${NC}"
+        content=$(curl -s "$target$endpoint")
+        echo "$content" > "$output_dir$endpoint"
+    else
+        echo -e "${RED}[-] $endpoint not found.${NC}"
+    fi
+}
+
 check_additional_info "$target" "$output_dir"
+
 
 # If nikto_scan is true, run Nikto
 if $nikto_scan; then
-    color_echo 33 "[ ! ] Executing Nikto scan, please wait..."
+    echo -e "${YELLOW}[!] Executing Nikto scan, please wait...${NC}"
     if [ -z "$proxy" ]; then
         nikto -host "$target" -o "$output_dir/nikto_output.txt"
     else
@@ -174,7 +184,7 @@ if $nikto_scan; then
     echo
 fi
 
-color_echo 33 "[ ! ] Executing $spider_tool, please wait..."
+echo -e "${YELLOW}[!] Executing $spider_tool, please wait...${NC}"
 
 if [ "$spider_tool" == "paramspider" ]; then
     # Run ParamSpider
@@ -197,8 +207,8 @@ wait
 echo
 
 # Display discovered vulnerabilities in a table-like format
-color_echo 32 "Count of potential vulnerable URLs discovered:"
-echo ""
+echo -e "${YELLOW}Count of potential vulnerable URLs discovered:${NC}"
+echo
 printf "%-15s %-25s\n" "Vulnerability" "URLs discovered"
 printf "%-15s %-25s\n" "-------------" "---------------"
 
@@ -218,12 +228,12 @@ done
 # Count the number of JS files discovered
 js_count=$(grep -Eo 'https?://[^/]+/[^ ]+\.js' "$getJS_output" | wc -l)
 echo
-color_echo 32 "JavaScript files discovered: $js_count"
+echo -e "${GREEN}JavaScript files discovered:${NC} $js_count"
 echo
 
 # Execute nuclei if the template is provided
 if [ ! -z "$template" ]; then
-    color_echo 33 "[ ! ] Executing nuclei with template $template, please wait..."
+    echo -e "${YELLOW}[!] Executing nuclei with template $template, please wait...${NC}"
     
     for file in $output_dir/paramspider/vuln/*.txt; do
         nuclei -l "$file" -t "$template" $rate_limit $proxy -o "$output_dir/nuclei_results.txt"
@@ -233,4 +243,4 @@ fi
 # Add some space for better formatting
 echo
 
-color_echo 32 "[+] Scan complete. Results saved in $output_dir"
+echo -e "${GREEN}[+] Scan complete. Results saved in $output_dir${NC}"
