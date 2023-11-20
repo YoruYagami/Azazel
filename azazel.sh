ee2abf1c40244c41c234a36eadb8777edfb3f682
YoruYagami <70035442+YoruYagami@users.noreply.github.com>
2023-11-20 13:48:12 +0100

    Update azazel.sh

--- azazel.sh
+++ azazel.sh
@@ -12,18 +12,17 @@ proxy=""
 target=""
 fuzz_enabled=0
 fuzz_template_path="~/fuzzing-templates" # Default path
+wordlist="/usr/share/seclists/Discovery/Web-Content/common.txt" # Default wordlist
 
 help_message() {
     echo
-    echo -e "Usage: $0 [./azazel.sh -d \"https://target.com\" --rl 15 -t ~/fuzzing-templates/ --proxy http://127.0.0.1:8080 --slow/--medium/--fast --fuzz [template path]]\n"
+    echo -e "Usage: $0 [./azazel.sh -d \"https://target.com\" -w /path/to/wordlist --rl [rate limit] -t ~/fuzzing-templates/ --proxy http://127.0.0.1:8080 --fuzz [template path]]\n"
     echo "Options:"
     echo "  -d                 Specify the target domain"
+    echo "  -w                 Specify the wordlist for Gobuster (default is common.txt)"
     echo "  -t                 Use Nuclei with a specific template"
-    echo "  --rl               Set global rate limit (e.g., --rl 15)"
+    echo "  --rl               Set global rate limit (e.g., --rl 10)"
     echo "  --proxy            Set proxy for selected tools (e.g., --proxy http://127.0.0.1:8080)"
-    echo "  --slow             Set rate limit for slow scanning"
-    echo "  --medium           Set rate limit for medium scanning (default)"
-    echo "  --fast             Set rate limit for fast scanning"
     echo "  --fuzz             Enable fuzzing with Dalfox and Nuclei using specified template path"
     echo "  -h                 Display this help message"
     echo
@@ -39,18 +38,17 @@ while :; do
             target="$2"
             shift
             ;;
+        -w)
+            wordlist="$2"
+            shift
+            ;;
         -t)
             template="$2"
             shift
             ;;
-        --slow)
-            rate_limit="--rl 5"
-            ;;
-        --medium)
-            rate_limit="--rl 50"
-            ;;
-        --fast)
-            rate_limit="--rl 150"
+        --rl)
+            rate_limit="--rl $2"
+            shift
             ;;
         --proxy)
             proxy="--proxy $2"
@@ -88,13 +86,13 @@ mkdir -p "$output_dir"
 # Running whatweb
 echo -e "${YELLOW}[!] Executing whatweb...${NC}"
 whatweb "$target" | tee "$output_dir/whatweb.txt"
-sleep 2
+wait
 echo
 
 # Running wafw00f
 echo -e "${YELLOW}[!] Executing wafw00f...${NC}"
 wafw00f "$target" | tee "$output_dir/wafw00f.txt"
-sleep 2
+wait
 echo
 
 # Checking for X-Frame-Options header
@@ -106,37 +104,64 @@ if ! curl -s -I "$target" | grep -qi "X-Frame-Options"; then
 else
     echo -e "${GREEN}[+] X-Frame-Options header present.${NC}"
 fi
-sleep 2
+wait
 echo
 
-echo -e "${YELLOW}[!] Executing katana, please wait...${NC}"
-katana -u "$target" -d 5 -kf robotstxt,sitemapxml -o "$output_dir/urls.txt" $rate_limit $proxy
-sleep 2
+# Execute Heartbleed check
+echo -e "${YELLOW}[!] Checking for Heartbleed vulnerability...${NC}"
+echo "QUIT" | openssl s_client -connect $target:443 2>&1 | grep 'server extension "heartbeat" (id=15)' || echo "$target: safe"
+wait
 echo
+
+# Execute Katana
+echo -e "${YELLOW}[!] Executing Katana and JS analysis, please wait...${NC}"
+katana -u "$target" -d 5 -kf robotstxt,sitemapxml -jc -o "$output_dir/urls.txt" $rate_limit $proxy
+wait
+echo
+
+# Extract JS and URLs from JS
+getJS --url $target --output "$output_dir/javascript_results.txt"
+if [ -s "$output_dir/javascript_results.txt" ]; then
+    while IFS= read -r js_file; do
+        curl "$js_file" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" >> "$output_dir/js_urls.txt"
+    done < "$output_dir/javascript_results.txt"
+fi
+wait
+echo
+
+# Check for SSL and set Gobuster flag
+ssl_check=$(curl -Is $target | grep -i 'Location: https')
+gobuster_tls=""
+if [ -z "$ssl_check" ]; then
+    gobuster_tls="-k"
+fi
 
 # Execute Gobuster
 echo -e "${YELLOW}[!] Executing Gobuster, please wait...${NC}"
-gobuster dir -u "$target" -w /usr/share/seclists/Discovery/Web-Content/common.txt -o "$output_dir/gobuster_results.txt" $proxy
-sleep 2
+gobuster dir -u "$target" -w "$wordlist" --quiet -o "$output_dir/gobuster_results.txt" $proxy
+wait
 echo
 
 # Execute Dalfox
+echo -e "${YELLOW}[!] Executing Dalfox, please wait...${NC}"
 if [ $fuzz_enabled -eq 1 ]; then
-    echo -e "${YELLOW}[!] Executing Dalfox with fuzzing, please wait...${NC}"
-    dalfox file "$output_dir/urls.txt" --mining-dom --deep-domxss --mining-dict --delay 500 --report $proxy -o "$output_dir/dalfox_fuzz_results.txt"
-    sleep 2
-    echo
+    grep -E "\?" "$output_dir/urls.txt" > "$output_dir/urls_with_params.txt"
+    dalfox file "$output_dir/urls_with_params.txt" --mining-dom --deep-domxss --mining-dict --silence $proxy -o "$output_dir/dalfox_fuzz_results.txt"
+else
+    dalfox url "$target" --mining-dom --deep-domxss --mining-dict --silence --report $proxy -o "$output_dir/dalfox_results.txt"
 fi
+wait
+echo
 
 # Execute nuclei if the template is provided
+echo -e "${YELLOW}[!] Executing nuclei, please wait...${NC}"
 if [ ! -z "$template" ] || [ $fuzz_enabled -eq 1 ]; then
-    echo -e "${YELLOW}[!] Executing nuclei with template $template, please wait...${NC}"
     if [ $fuzz_enabled -eq 1 ]; then
         nuclei -l "$output_dir/urls.txt" -t "$fuzz_template_path" $rate_limit $proxy -as -o "$output_dir/nuclei_fuzz_results.txt"
     else
         nuclei -u "$target" -t "$template" $rate_limit $proxy -as -o "$output_dir/nuclei_results.txt"
     fi
-    echo
 fi
+echo
 
 echo -e "${GREEN}[+] Scan complete. Results saved in $output_dir${NC}"
