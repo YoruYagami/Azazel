0374fb84b54fea6ac0737222f17455e6e2367f59
YoruYagami <70035442+YoruYagami@users.noreply.github.com>
2024-01-15 16:02:58 +0100

    Update azazel.sh

--- azazel.sh
+++ azazel.sh
@@ -11,21 +11,23 @@ NC='\033[0m' # No Color
 usage() {
     echo -e "${YELLOW}=================== ${GREEN}Script Usage${YELLOW} ===================${NC}"
     echo -e "${BLUE}Usage:${NC}"
-    echo -e "  $0 -f <domain_file> [--loop <loop_time>] [-o <output_file>] [-x <exclude_file>]\n"
+    echo -e "  $0 -f <domain_file> [--loop <loop_time>] [-o <output_file>] [-x <exclude_file>] [--silent]\n"
     
     echo -e "${YELLOW}Options:${NC}"
     echo -e "  ${GREEN}-f, --file${NC}    ${BLUE}Domain file to process${NC}"
     echo -e "  ${GREEN}--loop${NC}       ${BLUE}Time in seconds to wait before re-running the script${NC} ${RED}(default: 7200)${NC}"
     echo -e "  ${GREEN}-o, --output${NC}  ${BLUE}Output file for results${NC} ${RED}(default: subdomains.txt)${NC}"
-    echo -e "  ${GREEN}-x, --exclude${NC} ${BLUE}File with subdomains to exclude from results${NC}\n"
+    echo -e "  ${GREEN}-x, --exclude${NC} ${BLUE}File with subdomains to exclude from results${NC}"
+    echo -e "  ${GREEN}--silent${NC}     ${BLUE}Run subfinder, httpx, and notify in silent mode${NC}\n"
     echo -e "${YELLOW}=================================================${NC}"
     exit 1
 }
 
 domain_file=""
 loop_time=7200
-output_file="subdomains.txt"
+output_file="output.txt"
 exclude_file=""
+silent_mode=""
 
 parse_arguments() {
     while [[ $# -gt 0 ]]; do
@@ -45,6 +47,10 @@ parse_arguments() {
             -x|--exclude)
                 exclude_file="$2"
                 shift 2
+                ;;
+            --silent)
+                silent_mode="-silent"
+                shift
                 ;;
             -h|--help)
                 usage
@@ -66,17 +72,22 @@ if [[ -z "$domain_file" ]]; then
     usage
 fi
 
-# Read domains from the file
-readarray -t targets < "$domain_file"
-
 while :
 do
-    for target in "${targets[@]}"
-    do
-        subfinder -d "$target" -o "$output_file"
-    done
+    cat "$domain_file" | assetfinder -subs-only > sub1
+    findomain -q -f "$domain_file" > sub2
+    subfinder -dL "$domain_file" -o sub3 -all -nW $silent_mode
+    cat sub1 sub2 sub3 | sort -u > "$output_file"
+    rm -rf sub1 sub2 sub3
 
-    cat "$output_file" | anew old_subdomains.txt | httpx -silent | notify -p telegram -id bot_name "New subdomains found for ${targets>
-    > "$output_file"  # Clear the temporary file for new subdomains
-    sleep "$loop_time"  # Wait for the specified time before running the loop again
+    # Exclude subdomains from the exclude file if provided
+    if [[ -n "$exclude_file" ]]; then
+        grep -v -f "$exclude_file" "$output_file" > temp.txt
+        mv temp.txt "$output_file"
+    fi
+
+    cat "$output_file" | anew subdomains.txt | httpx $silent_mode | notify -p telegram $silent_mode
+    > "$output_file"
+    rm -rf subdomains.txt
+    sleep "$loop_time" 
 done
