d539bd0ccbfdd7ff3e369e6131e83e3a41c1178f
YoruYagami <70035442+YoruYagami@users.noreply.github.com>
2024-01-15 18:20:33 +0100

    Update azazel.sh

--- azazel.sh
+++ azazel.sh
@@ -18,7 +18,9 @@ usage() {
     echo -e "  ${GREEN}--loop${NC}       ${BLUE}Time in seconds to wait before re-running the script${NC} ${RED}(default: 7200)${NC}"
     echo -e "  ${GREEN}-o, --output${NC}  ${BLUE}Output file for results${NC} ${RED}(default: subdomains.txt)${NC}"
     echo -e "  ${GREEN}-x, --exclude${NC} ${BLUE}File with subdomains to exclude from results${NC}"
-    echo -e "  ${GREEN}--silent${NC}     ${BLUE}Run subfinder, httpx, and notify in silent mode${NC}\n"
+    echo -e "  ${GREEN}--silent${NC}     ${BLUE}Run subfinder, httpx, and notify in silent mode${NC}"
+    echo -e "  ${GREEN}-n, --nuclei${NC}     ${BLUE}Run nuclei against newly discovered live subdomains${NC}"
+    echo -e "  ${GREEN}-t, --template${NC}     ${BLUE}Path to nuclei templates${NC} ${RED}(default: ~/nuclei-templates)${NC}\n"
     echo -e "${YELLOW}=================================================${NC}"
     exit 1
 }
@@ -28,6 +30,8 @@ loop_time=7200
 output_file="output.txt"
 exclude_file=""
 silent_mode=""
+nuclei_mode=false
+template_path=~/nuclei-templates
 
 parse_arguments() {
     while [[ $# -gt 0 ]]; do
@@ -52,6 +56,14 @@ parse_arguments() {
                 silent_mode="-silent"
                 shift
                 ;;
+            -n|--nuclei)
+                nuclei_mode=true
+                shift
+                ;;
+            -t|--template)
+                template_path="$2"
+                shift 2
+                ;;
             -h|--help)
                 usage
                 ;;
@@ -74,20 +86,43 @@ fi
 
 while :
 do
+    # Using assetfinder to find subdomains and output to sub1
     cat "$domain_file" | assetfinder -subs-only > sub1
+
+    # Using findomain to find subdomains and output to sub2
     findomain -q -f "$domain_file" > sub2
+
+    # Using subfinder to find subdomains and output to sub3
     subfinder -dL "$domain_file" -o sub3 -all -nW $silent_mode
+
+    # Combining outputs from all tools, sort them uniquely, and store in output file
     cat sub1 sub2 sub3 | sort -u > "$output_file"
+
+    # Clean up temporary files
     rm -rf sub1 sub2 sub3
 
-    # Exclude subdomains from the exclude file if provided
+    # Exclude subdomains listed in the exclude file if provided
     if [[ -n "$exclude_file" ]]; then
         grep -v -f "$exclude_file" "$output_file" > temp.txt
         mv temp.txt "$output_file"
     fi
 
-    cat "$output_file" | anew subdomains.txt | httpx $silent_mode | notify -p telegram $silent_mode
+    # Process new subdomains with anew and store them in a temporary file
+    cat "$output_file" | anew subdomains.txt > new_subdomains.txt
+
+    # Process with httpx and notify
+    cat new_subdomains.txt | httpx $silent_mode -status-code -tech-detect -title | notify $silent_mode
+
+    naabu -host 
+    
+    # Run nuclei on the new subdomains if nuclei_mode is true
+    if $nuclei_mode; then
+        nuclei -l new_subdomains.txt -t "$template_path" -o nuclei_output.txt -as | notify $silent_mode
+    fi
+
+    # Clear the output file for the next iteration
     > "$output_file"
-    rm -rf subdomains.txt
+
+    # Wait for specified loop time before next iteration
     sleep "$loop_time" 
 done
