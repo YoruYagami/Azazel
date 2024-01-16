bef9ab79b6b90af788701854a216cb79059ca4c0
YoruYagami <aw4y0ut@protonmail.com>
2024-01-16 11:45:53 -0500

    new required tools update

--- install.sh
+++ install.sh
@@ -6,30 +6,6 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-# Install ParamSpider
-if ! command -v paramspider &> /dev/null; then
-    echo -e "${RED}Installing ParamSpider...${NC}"
-    git clone https://github.com/devanshbatham/ParamSpider.git "$HOME/ParamSpider"
-    cd $HOME/ParamSpider
-    pip install .
-    rm -rf "$HOME/ParamSpider"
-    echo -e "${GREEN}ParamSpider has been installed.${NC}"
-else
-    echo -e "${GREEN}ParamSpider is already installed.${NC}"
-fi
-
-if ! command -v openredirex &> /dev/null; then
-    echo -e "${RED}Installing openredirex...${NC}"
-    git clone https://github.com/devanshbatham/openredirex "$HOME/openredirex"
-    cd $HOME/openredirex
-    sudo chmod +x setup.sh
-    ./setup.sh
-    rm -rf "$HOME/openredirex"
-    echo -e "${GREEN}openredirex has been installed.${NC}"
-else
-    echo -e "${GREEN}openredirex is already installed.${NC}"
-fi
-
 # Install Katana
 if ! command -v katana &> /dev/null; then
     echo -e "${RED}Installing Katana...${NC}"
@@ -39,34 +15,60 @@ else
     echo -e "${GREEN}Katana is already installed.${NC}"
 fi
 
-# Set the destination directory for gf-patterns
-mkdir -p "$HOME/.gf"
-TARGET_DIR="$HOME/.gf"
+# Install assetfinder
+if ! command -v assetfinder &> /dev/null; then
+    echo -e "${RED}Installing assetfinder...${NC}"
+    go install github.com/tomnomnom/assetfinder@latest
+    echo -e "${GREEN}assetfinder has been installed.${NC}"
+else
+    echo -e "${GREEN}assetfinder is already installed.${NC}"
+fi
 
-# Clone each repository and search for JSON patterns
-for repo in \
-    # Your list of repositories here
-do
-    echo -e "${YELLOW}Cloning $repo...${NC}"
-    
-    # Check if the repository is public
-    if curl -s -I "$repo" | grep -q "HTTP/.* 200"; then
-        # Clone the repository with the --depth 1 option to only download the latest commit
-        git clone --depth 1 "$repo"
+# Install findomain
+if ! command -v findomain &> /dev/null; then
+    echo -e "${RED}Installing findomain...${NC}"
+    go install github.com/Edu4rdSHL/findomain@latest
+    echo -e "${GREEN}findomain has been installed.${NC}"
+else
+    echo -e "${GREEN}findomain is already installed.${NC}"
+fi
 
-        # Search for JSON patterns recursively
-        find . -name "*.json" -exec mv {} "$TARGET_DIR" \; 2>/dev/null
-        find . -name "*.JSON" -exec mv {} "$TARGET_DIR" \; 2>/dev/null
-        find . -name "*.geojson" -exec mv {} "$TARGET_DIR" \; 2>/dev/null
-        find . -name "*.GeoJSON" -exec mv {} "$TARGET_DIR" \; 2>/dev/null
+# Install subfinder
+if ! command -v subfinder &> /dev/null; then
+    echo -e "${RED}Installing subfinder...${NC}"
+    go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+    echo -e "${GREEN}subfinder has been installed.${NC}"
+else
+    echo -e "${GREEN}subfinder is already installed.${NC}"
+fi
 
-        # Remove the cloned repository
-        echo -e "${RED}Removing $repo...${NC}"
-        rm -rf $(basename "$repo")
-    else
-        echo -e "${RED}$repo is no longer public or has been deleted. Moving to the next...${NC}"
-    fi
-done
+# Install httpx
+if ! command -v httpx &> /dev/null; then
+    echo -e "${RED}Installing httpx...${NC}"
+    go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+    echo -e "${GREEN}httpx has been installed.${NC}"
+else
+    echo -e "${GREEN}httpx is already installed.${NC}"
+fi
+
+# Install anew
+if ! command -v anew &> /dev/null; then
+    echo -e "${RED}Installing anew...${NC}"
+    go install github.com/tomnomnom/anew@latest
+    echo -e "${GREEN}anew has been installed.${NC}"
+else
+    echo -e "${GREEN}anew is already installed.${NC}"
+fi
+
+# Install nuclei
+if ! command -v nuclei &> /dev/null; then
+    echo -e "${RED}Installing nuclei...${NC}"
+    go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest 
+    echo -e "${GREEN}nuclei has been installed.${NC}"
+else
+    echo -e "${GREEN}nuclei is already installed.${NC}"
+fi
+
 
 echo -e "${GREEN}All tasks have been completed successfully.${NC}"
 
